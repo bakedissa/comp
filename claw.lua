5a2effb0c44be722053edffc337afca370a93399
bakedissa <lonlyalt3b@gmail.com>
2025-09-22 20:10:54 -0400

    Create claw.lua

---
+++ claw.lua
@@ -0,0 +1,4 @@
+getgenv().reportTime = 63243243242340 --when it reports in seconds, set to big number if no want report
+getgenv().tryCollectMultipleTimes = false -- set to true to try and collect items multiple times, makes the script slower and I've not seen much difference but try it yourself 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IdiotHub/Scripts/refs/heads/main/BGSI/AutoClaw.lua"))()
